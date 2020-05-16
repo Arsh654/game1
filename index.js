68e86b7bc2be03293cbd84a4f9dfd429791063c5
Arshad <47123457+Arsh654@users.noreply.github.com>
2020-05-16 16:53:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src" , randomImageSource2);
 
 if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "🚩 player 1 wins";
+  document.querySelector("h1").innerHTML = "🚩player 1 wins";
 }
 else if (randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = "🚩 player 2 wins";
+  document.querySelector("h1").innerHTML = "🚩player 2 wins";
 }
 else {
   document.querySelector("h1").innerHTML = "TRY AGAIN";
