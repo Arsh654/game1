7fa2e2a746b281f602a0c198d629b89cd57c1ea8
Arshad <47123457+Arsh654@users.noreply.github.com>
2020-05-25 23:01:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ if (randomNumber1 > randomNumber2){
   document.querySelector("h1").innerHTML = "🚩" + player1 + " wins";
 }
 else if (randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = "🚩" + player2  + " wins";
+  document.querySelector("h1").innerHTML = "🚩" + player2  +  " wins";
 }
 else {
   document.querySelector("h1").innerHTML = "TRY AGAIN";
