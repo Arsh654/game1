13bc0aa9a263b48b998e030ca56dbfadccecda61
Arshad <47123457+Arsh654@users.noreply.github.com>
2020-05-25 22:58:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var player1 = prompt("Enter 1st Player Name:");
+
+var player2 = prompt("Enter 2nd Player Name:");
 
 var randomNumber1 = Math.floor(Math.random() * 6) + 1; //random no. from 1-6
 
@@ -20,10 +23,10 @@ var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src" , randomImageSource2);
 
 if (randomNumber1 > randomNumber2){
-  document.querySelector("h1").innerHTML = "🚩player 1 wins";
+  document.querySelector("h1").innerHTML = "🚩" + player1 + "wins";
 }
 else if (randomNumber2 > randomNumber1){
-  document.querySelector("h1").innerHTML = "🚩player 2 wins";
+  document.querySelector("h1").innerHTML = "🚩" + player2  + "wins";
 }
 else {
   document.querySelector("h1").innerHTML = "TRY AGAIN";
